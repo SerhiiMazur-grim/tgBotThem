fb32314bc1533e6e47296714419bcf12c58a784a
Grimnebuulen <mr.trader92@gmail.com>
2024-06-25 13:18:43 +0300

    add ForbiddenErrorMiddleware

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -2,8 +2,9 @@ import sqlalchemy.orm as orm
 from aiogram import Dispatcher
 
 from .user_mw import UserMiddleware
+from .exeption_mw import ForbiddenErrorMiddleware
 
 
 def setup(dp: Dispatcher, sessionmaker: orm.sessionmaker):
-    
+    dp.update.outer_middleware(ForbiddenErrorMiddleware())
     dp.update.outer_middleware(UserMiddleware(sessionmaker))
